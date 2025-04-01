fb4cc1d4e860ea564d5a797bdadd10863fbe3ec6
YavorPenev <yavorpen@gmail.com>
2025-04-02 01:51:31 +0300

    fix footer

--- Frontend/src/home.jsx
+++ Frontend/src/home.jsx
@@ -174,7 +174,20 @@ function Home() {
                   </Link>
                 </div>
 
-                <img className="w-full" src="/mainpic-bottom.png" alt="Footer Visual" />
+                <footer className="flex bg-blue-800 bg-gradient-to-b from-transparent to-gray-800 text-blue-100 p-5 justify-evenly flex-wrap items-center">
+        <p>©legends Development Team</p>
+        <Link className='hover:underline' to="/learnmore">Learn More Page</Link>
+        <Link className='hover:underline' to="/articles">Articles Page</Link>
+        <Link className='align-baseline hover:underline' to="/faq">FAQ Page</Link>
+        <div className='flex-col flex-nowrap justify-evenly gap-2 items-center justify-items-center'>
+          <a><img className='aspect-auto h-15' src='/youtube.png'/></a>
+          <p>+39 06 6988 4857</p>
+          <p>yavorpen@gmail.com</p>
+        </div>
+        <Link className='hover:underline' to="/stockai">To Stocks AI Assistant</Link>
+        <Link to="/about" className="hover:underline">About</Link>
+  </footer>
+
               </>
             }
           />
