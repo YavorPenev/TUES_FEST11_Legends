f29bdd51c7f1f7cebec38778afe74c02c14c4503
YavorPenev <yavorpen@gmail.com>
2025-04-02 01:34:49 +0300

    fix merge, add invest, advice, investcal, Calculators, CurrencyCalculato

--- Frontend/src/home.jsx
+++ Frontend/src/home.jsx
@@ -1,3 +1,4 @@
+
 import { useState, useRef } from 'react';
 import { Routes, Route, useNavigate, Link } from 'react-router-dom';
 import About from './about';
@@ -28,7 +29,7 @@ function Home() {
 
   return (
     <>
-      {/* тук Хедърът се показва на всички страници */}
+      {/* Хедърът, който ще се вижда на всички страници */}
       <header className="pl-5 pr-5 fixed top-0 left-0 w-full bg-blue-800 border-blue-900 flex justify-between p-4 border-b-4 bg-gradient-to-b from-gray-800 to-transparent items-center z-50">
         <Link to="/" className="bg-blue-100 pr-[1%] pl-[1%] pt-[0.4%] pb-[0.4%] rounded-xl hover:scale-110 transition-transform duration-200">
           <img
@@ -41,25 +42,25 @@ function Home() {
 
         <h1 className="text-white font-bold text-3xl ml-4">SmartBudget</h1>
         <Link
-          to='/articles'
+          to="/articles"
           className="bg-blue-100 pl-3 pr-3 pt-2 pb-2 rounded-2xl text-xl text-blue-950 font-bold hover:scale-110 transition-transform duration-200"
         >
           Articles
         </Link>
         <Link
-          to='/login'
+          to="/login"
           className="bg-blue-100 pl-3 pr-3 pt-2 pb-2 rounded-2xl text-xl text-blue-950 font-bold hover:scale-110 transition-transform duration-200"
         >
           Login
         </Link>
         <Link
-          to='/signup'
+          to="/signup"
           className="bg-blue-100 pl-3 pr-3 pt-2 pb-2 rounded-2xl text-xl text-blue-950 font-bold hover:scale-110 transition-transform duration-200"
         >
           Sign Up
         </Link>
         <Link
-          to='/account'
+          to="/account"
           className="bg-blue-100 pl-3 pr-3 pt-2 pb-2 rounded-2xl text-xl text-blue-950 font-bold hover:scale-110 transition-transform duration-200"
         >
           My Account
@@ -70,7 +71,6 @@ function Home() {
           onClick={CalcMenuChange}
         >
           <img src="/calclogo.png" alt="Calculator" className="h-7" />
-
         </button>
 
         <div
@@ -81,89 +81,96 @@ function Home() {
           <Link
             className="bg-blue-100 text-blue-950 rounded-lg p-2 hover:scale-110 transition-transform duration-200"
             to="/calcloan"
-            title='Advanced Loan Calculator'
+            title="Advanced Loan Calculator"
           >
             Loan
           </Link>
           <Link
             className="bg-blue-100 text-blue-950 rounded-lg p-2 hover:scale-110 transition-transform duration-200"
             to="/calcstocks"
-            title='Stocks Profit Calculator'
+            title="Stocks Profit Calculator"
           >
             Stocks
           </Link>
           <Link
             className="bg-blue-100 text-blue-950 rounded-lg p-2 hover:scale-110 transition-transform duration-200"
             to="/calcexch"
-            title='Currency Exchange Calculator'
+            title="Currency Exchange Calculator"
           >
             Currency
           </Link>
         </div>
 
         <Link
-          to='/faq'
+          to="/faq"
           className="bg-blue-100 pt-2 pb-2 pr-5 pl-5 rounded-2xl text-2xl font-extrabold text-blue-950 hover:scale-110 transition-transform duration-200"
         >
           ?
         </Link>
       </header>
 
-      {/* AI Menu */}
-      <div
-        ref={aimenuref}
-        className="flex-col-reverse rounded-t-full fixed right-5 bottom-17 pb-15 pt-5 gap-4 text-blue-100 w-25 bg-blue-800 border-4 border-blue-950"
-        style={{ display: 'none' }}
-      >
-        <Link
-          className="w-[80%] ml-[10%] mr-[10%] bg-blue-100 text-blue-950 rounded-xl pt-2 pb-2 hover:scale-110 transition-transform duration-200"
-          to="/stockai"
-          title='Helps users with investing and selling stocks on the market by inputing goals, income and taking into account the probability of different companies` success'
-        >
-          Stocks<br />
-          Advisor
-        </Link>
-        <Link
-          className="w-[80%] ml-[10%] mr-[10%] bg-blue-100 text-blue-950 rounded-xl pt-2 pb-2 hover:scale-110 transition-transform duration-200"
-          to="/budgetai"
-          title='Input income and expenses to get a recommended budget breakdown'
-        >
-          Budget<br />
-          Planner
-        </Link>
-      </div>
 
-      {/* AI Button */}
-      <button
-        className="w-25 h-25 text-3xl pb-23 font-extrabold rounded-full border-blue-950 border-4 bg-blue-800 text-blue-100 fixed bottom-5 right-5 cursor-pointer hover:scale-110 transition-transform duration-200"
-        onClick={AiMenuChange}
-      >
-        <sub><sub>^</sub></sub>
-        <br/>
-        <b>AI</b>
-      </button>
+      <div className="pt-20">
+        <Routes>
+          {/* Главна страница */}
+          <Route
+            path="/"
+            element={
+              <>
+ 
+                <div
+                  ref={aimenuref}
+                  className="flex-col-reverse rounded-t-full fixed right-5 bottom-17 pb-15 pt-5 gap-4 text-blue-100 w-25 bg-blue-800 border-4 border-blue-950"
+                  style={{ display: 'none' }}
+                >
+                  <Link
+                    className="w-[80%] ml-[10%] mr-[10%] bg-blue-100 text-blue-950 rounded-xl pt-2 pb-2 hover:scale-110 transition-transform duration-200"
+                    to="/stockai"
+                  >
+                    Stocks<br />
+                    Advisor
+                  </Link>
+                  <Link
+                    className="w-[80%] ml-[10%] mr-[10%] bg-blue-100 text-blue-950 rounded-xl pt-2 pb-2 hover:scale-110 transition-transform duration-200"
+                    to="/budgetai"
+                  >
+                    Budget<br />
+                    Planner
+                  </Link>
+                </div>
 
-      <img
-        className="mt-2 w-full border-b-8 border-b-blue-900"
-        src="/mainpic-above.png"
-      />
+                <button
+                  className="w-25 h-25 text-3xl pb-23 font-extrabold rounded-full border-blue-950 border-4 bg-blue-800 text-blue-100 fixed bottom-5 right-5 cursor-pointer hover:scale-110 transition-transform duration-200"
+                  onClick={AiMenuChange}
+                >
+                  <sub>^</sub>
+                  <br />
+                  <b>AI</b>
+                </button>
 
-      <div className="flex justify-center items-center pb-5 pt-5 w-full flex-col bg-gradient-to-b from-blue-100 to-gray-400 border-b-8 border-blue-900">
-        <img className="w-[30%]" src="/SmartBudget.png" />
-        <h1 className="text-4xl font-semibold text-blue-900">
-          Your Ultimate Financial Knowledge Hub
-        </h1>
-        <Link
-          to='/learnmore'
-          className="bg-blue-800 mt-10 pl-3 pr-3 pt-2 pb-2 rounded-2xl text-2xl text-blue-100 font-bold hover:scale-110 transition-transform duration-200"
-        >
-          Learn More
-        </Link>
-      </div>
 
-          <img className="w-full" src="/mainpic-bottom.png" />
+                <img
+                  className="mt-2 w-full border-b-8 border-b-blue-900"
+                  src="/mainpic-above.png"
+                  alt="Main Visual"
+                />
 
-      <footer className="flex bg-blue-800 bg-gradient-to-b from-transparent to-gray-800 text-blue-100 p-5 justify-evenly flex-wrap items-center">
+                <div className="flex justify-center items-center pb-5 pt-5 w-full flex-col bg-gradient-to-b from-blue-100 to-gray-400 border-b-8 border-blue-900">
+                  <img className="w-[30%]" src="/SmartBudget.png" alt="SmartBudget Logo" />
+                  <h1 className="text-4xl font-semibold text-blue-900">
+                    Your Ultimate Financial Knowledge Hub
+                  </h1>
+                  <Link
+                    to="/learnmore"
+                    className="bg-blue-800 mt-10 pl-3 pr-3 pt-2 pb-2 rounded-2xl text-2xl text-blue-100 font-bold hover:scale-110 transition-transform duration-200"
+                  >
+                    Learn More
+                  </Link>
+                </div>
+
+                <img className="w-full" src="/mainpic-bottom.png" alt="Footer Visual" />
+
+                <footer className="flex bg-blue-800 bg-gradient-to-b from-transparent to-gray-800 text-blue-100 p-5 justify-evenly flex-wrap items-center">
         <p>©legends Development Team</p>
         <Link className='hover:underline' to="/learnmore">Learn More Page</Link>
         <Link className='hover:underline' to="/articles">Articles Page</Link>
@@ -175,7 +182,16 @@ function Home() {
         </div>
         <Link className='hover:underline' to="/stockai">To Stocks AI Assistant</Link>
         <Link to="/about" className="hover:underline">About</Link>
-      </footer>
+  </footer>
+              </>
+            }
+          />
+          {/*tuk se swyrzwat wsi`ki stranci  */}
+          <Route path="/about" element={<About />} />
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="/law" element={<Law />} />
+        </Routes>
+      </div>
     </>
   );
 }
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,15 +2,15 @@ import { React, StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import Home from './home';
-import About from './about';
+
 import './styles/index.css';
 
+//tuk se importwa samo home
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/*" element={<Home />} />
-        <Route path='/about' element={<About/>}/>
       </Routes>
     </BrowserRouter>
   </StrictMode>
