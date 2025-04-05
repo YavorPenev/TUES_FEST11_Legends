40f1c63b64730693f510b3948876525d2378c80b
Bobi-77 <borislavstojnev8@gmail.com>
2025-04-05 22:28:17 +0300

    footer-fix

--- Frontend/src/footer.jsx
+++ Frontend/src/footer.jsx
@@ -3,23 +3,34 @@ import { Link } from "react-router";
 
 function Footer() {
     return (
-        <footer className="absolute w-full flex bg-blue-800 bg-gradient-to-b from-transparent to-gray-800 text-blue-100 p-5 justify-evenly flex-wrap items-center">
-            <p>©legends Development Team</p>
-            <Link className="hover:underline" to="/learnmore">Learn More Page</Link>
-            <Link className="hover:underline" to="/articles">Articles Page</Link>
-            <Link className="align-baseline hover:underline" to="/faq">FAQ Page</Link>
-            <div className="flex-col flex-nowrap justify-evenly gap-2 items-center justify-items-center">
-                <a
-                    href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                ><img className="aspect-auto h-15" src="/youtube.png" alt="YouTube" /></a>
-                <p>+39 06 6988 4857</p>
-                <p>yavorpen@gmail.com</p>
-            </div>
-            <Link className="hover:underline" to="/stockai">To Stocks AI Assistant</Link>
-            <Link to="/about" className="hover:underline">About</Link>
-        </footer>
+        <footer className="flex bg-blue-800 bg-gradient-to-b from-transparent to-gray-800 text-blue-100 p-5 justify-evenly flex-wrap items-center">
+        <div className="flex-col flex-nowrap justify-evenly gap-2 items-center justify-items-center">
+          <a
+            href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img className="aspect-auto h-15" src="/youtube.png" alt="YouTube" />
+          </a>
+          <p>Copyright @2025</p>
+          <p>©legends Development Team</p>
+        </div>
+        <div className="flex-col flex-nowrap justify-evenly gap-2 items-center justify-items-center">
+          <p>legends@gmail.com</p>
+          <p>+39 06 6988 4857</p>
+          <p>+39 04 5355 9832</p>
+        </div>
+        <div className="flex flex-col gap-1 items-center">
+        <Link to="/investcalc" className="text-blue-100 hover:underline">Investment Calculator</Link>
+          <Link to="/calcloan" className="text-blue-100 hover:underline">Loan Calculator</Link>
+          <Link to="/CurrencyCalculator" className="text-blue-100 hover:underline">Currency Calculator</Link>
+        </div>
+        <div className="flex flex-col gap-1 items-start">
+          <Link to="/articles" className="text-blue-100 hover:underline">Articles</Link>
+          <Link to="/about" className="text-blue-100 hover:underline">About Us</Link>
+          <Link to="/law" className="text-blue-100 hover:underline">Terms of Use</Link>
+        </div>
+      </footer>
     );
 }
 
