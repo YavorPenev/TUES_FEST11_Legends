9e7d8459ff636a353809454b13eee10f62d0293c
Bobi-77 <borislavstojnev8@gmail.com>
2025-04-19 16:18:53 +0300

    profile-change

--- Frontend/src/InvCalc.jsx
+++ Frontend/src/InvCalc.jsx
@@ -1,10 +1,8 @@
-
 import React, { useState } from "react";
 import Header from './assets/header';
-import Footer from './assets/footer'; 
-import Calculator from './assets/SimpCalc'; 
+import Footer from './assets/footer';
+import Calculator from './assets/SimpCalc';
 import Notes from './assets/notes';
-
 
 function InvestCalc() {
   const [buyPrice, setBuyPrice] = useState("");
@@ -37,59 +35,61 @@ function InvestCalc() {
 
   return (
     <>
-      <Header /> 
-      <div className="mt-35 mb-10 max-w-md mx-auto p-4 rounded-2xl shadow-lg bg-white">
-        <h1 className="text-2xl font-bold mb-4 text-center">Stock/ETF Returns 📊</h1>
+      <div className="bg-gray-100 flex flex-col min-h-screen relative">
+        <Header />
+        <div className="flex-1 mt-35 mb-30 max-w-md mx-auto p-4 rounded-2xl shadow-lg bg-white mt-44.5">
+          <h1 className="text-2xl font-bold mb-4 text-center">Stock/ETF Returns 📊</h1>
 
-        <div className="space-y-2">
-          <input
-            type="number"
-            placeholder="Purchase Price"
-            value={buyPrice}
-            onChange={(e) => setBuyPrice(e.target.value)}
-            className="border p-2 w-full rounded mb-5"
-          />
-          <input
-            type="number"
-            placeholder="Current Price"
-            value={currentPrice}
-            onChange={(e) => setCurrentPrice(e.target.value)}
-            className="border p-2 w-full rounded  mb-5"
-          />
-          <input
-            type="number"
-            placeholder="Dividend per Stock"
-            value={dividend}
-            onChange={(e) => setDividend(e.target.value)}
-            className="border p-2 w-full rounded  mb-5"
-          />
-          <input
-            type="number"
-            placeholder="Number of Stocks"
-            value={shares}
-            onChange={(e) => setShares(e.target.value)}
-            className="border p-2 w-full rounded  mb-6" 
-          />
+          <div className="space-y-2">
+            <input
+              type="number"
+              placeholder="Purchase Price"
+              value={buyPrice}
+              onChange={(e) => setBuyPrice(e.target.value)}
+              className="border p-2 w-full rounded mb-5"
+            />
+            <input
+              type="number"
+              placeholder="Current Price"
+              value={currentPrice}
+              onChange={(e) => setCurrentPrice(e.target.value)}
+              className="border p-2 w-full rounded mb-5"
+            />
+            <input
+              type="number"
+              placeholder="Dividend per Stock"
+              value={dividend}
+              onChange={(e) => setDividend(e.target.value)}
+              className="border p-2 w-full rounded mb-5"
+            />
+            <input
+              type="number"
+              placeholder="Number of Stocks"
+              value={shares}
+              onChange={(e) => setShares(e.target.value)}
+              className="border p-2 w-full rounded mb-6"
+            />
 
-          <button
-            onClick={calculate}
-            className="bg-blue-500 text-white w-full p-2 rounded hover:bg-blue-600 z-index: 9999;"
-          >
-            Calculate 📈
-          </button>
-        </div>
-
-        {result && (
-          <div className="mt-4 space-y-2 text-lg">
-            <p>📊 Profit: <strong>{result.profit} BGN</strong></p>
-            <p>💸 Dividend Income: <strong>{result.dividend} BGN</strong></p>
-            <p>📈 Return: <strong>{result.returnPercent} %</strong></p>
+            <button
+              onClick={calculate}
+              className="bg-blue-500 text-white w-full p-2 rounded hover:bg-blue-600"
+            >
+              Calculate 📈
+            </button>
           </div>
-        )}
+
+          {result && (
+            <div className="mt-4 space-y-2 ">
+              <p>📊 Profit: <strong>{result.profit} BGN</strong></p>
+              <p>💸 Dividend Income: <strong>{result.dividend} BGN</strong></p>
+              <p>📈 Return: <strong>{result.returnPercent} %</strong></p>
+            </div>
+          )}
+        </div>
+        <Notes />
+        <Calculator />
+        <Footer className="fixed bottom-0 left-0 w-full bg-blue-800 text-white" />
       </div>
-      <Notes/>
-      <Calculator/>
-      <Footer /> 
     </>
   );
 }
